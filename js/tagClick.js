54e3ac843ba8f1be26c9a3894611efedf3c3ea6c
comjoy91 <comjoy91@gmail.com>
2017-06-08 19:50:13 +0900

    but safari에서는 더욱 느려짐. 원인 발견 및 수정 필요.

--- js/tagClick.js
+++ js/tagClick.js
@@ -4,6 +4,9 @@
 var clickedTagClass = undefined;
 var prevTagClass = undefined;
 
+var button_width = $('hashtag div.button').innerWidth();
+var button_height = $('hashtag div.button').innerHeight();
+
 $(document).ready(function(){
   $('#timeline').on('click', 'hashtag div.button', function(event) {
     event.preventDefault();
@@ -14,7 +17,7 @@ $(document).ready(function(){
       }
     console.log("click");
 
-    var article_height = $('#timeline').innerHeight();
+    var article_height_ratio = $('#timeline').innerHeight()/button_height;
 
     prevTagClass = clickedTagClass;
     clickedTagClass = $(this).parent().attr("class");
@@ -23,6 +26,10 @@ $(document).ready(function(){
     if (prevTagClass != undefined) {
       $.each($('hashtag.'+prevTagClass), function(index, tag) {
         var article = $(tag).parent().parent();
+        var article_width_ratio = ($(article).innerWidth()+2)/button_width;
+        var tag_top = $(tag).position().top;
+        var tag_left = $(tag).position().left;
+
         // folding background.
         $('.prevjump .arrowDot, .nextjump .arrowDot', article).css('fill', '#31454e');
         $('.prevjump, .nextjump', article).removeClass("tagSelected");
@@ -30,25 +37,17 @@ $(document).ready(function(){
         $(tag)
         .css('z-index', '-1')
         .children("div.background")
-        .css({
-          top: '',
-          height: '',
-          'transition-duration': ''
-        })
+        .css({ transform: 'translate(-'+tag_left+'px, 0px) scale('+article_width_ratio+', 1) ' })
         .one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) {
 //              $(this).parent().removeClass('selected');
           $(this).parent().css('z-index', '0');
-          $(this).css({
-            left: '',
-            width: ''
-          })
+          $(this).css('transform', '')
           .one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) {
             $(this).parent().removeAttr('style');
           });
         });
       });
     }
-
 
     // if you clicked the hashtag already chosen, whole job will be finished as it dis-selects the hashtag.
     if (prevTagClass == clickedTagClass) {
@@ -59,15 +58,14 @@ $(document).ready(function(){
     // now, let's expand the other background: which is clicked right now.
     $.each($('hashtag.'+clickedTagClass), function(index, tag) {
       var article = $(tag).parent().parent();
-      var article_width = $(article).innerWidth();
+      var article_width_ratio = ($(article).innerWidth()+2)/button_width;
       var tag_top = $(tag).position().top;
       var tag_left = $(tag).position().left;
 
       $(tag)
       .css('z-index', '0')
       .children("div.background").css({
-        left: -tag_left,
-        width: article_width
+        transform: 'translate(-'+tag_left+'px, 0px) scale('+article_width_ratio+', 1) ',
       })
       .one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) {
         $('.prevjump .arrowDot, .nextjump .arrowDot', article).css('fill', $('.'+clickedTagClass).css('background-color'));
@@ -75,14 +73,12 @@ $(document).ready(function(){
 //            $(this).parent().addClass('selected');
         $(this).parent().css('z-index', '-1');
         $(this).css({
-            top: -tag_top,
-            height: article_height
+            transform: 'translate(-'+tag_left+'px, -'+tag_top+'px) scale('+article_width_ratio+ ', '+article_height_ratio+')'
           })
           .one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) {
             $(this).parent().css('z-index', '0');
           });
       });
-
     });
     return 0;
 
